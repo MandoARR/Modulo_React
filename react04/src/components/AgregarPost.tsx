4c53bb4c99e4305b0d24e02b696ccf8ba2c1eb05
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-03-20 11:28:49 -0600

    acomodo y personalizacion del formulario

--- react04/src/components/AgregarPost.tsx
+++ react04/src/components/AgregarPost.tsx
@@ -3,11 +3,14 @@ function AgregarPost() {
         <>
             <div className="mb-3">
                 <label htmlFor="exampleFormControlInput1" className="form-label">Titulo</label>
-                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Nombre del Titulo"/>
+                <input type="text" className="form-control border-success" id="exampleFormControlInput1" placeholder="Nombre del Titulo"/>
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleFormControlTextarea1" className="form-label">Descripcion</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows={3}></textarea>
+                <textarea className="form-control bg-info bg-opacity-10 
+                border border-info border-start-0 rounded-end" 
+                id="exampleFormControlTextarea1" 
+                rows={3}></textarea>
             </div>
         </>
     )
