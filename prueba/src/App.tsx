7bb99622a03828d1f164c88756236bae169ca643
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-04-26 12:51:31 -0600

    Ejercicio de practica
    Se practico el ejercicio de crear un boton para generar 2 numeros aleatorios para familiarizarse con el codigo y los hooks

---
+++ prueba/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { NumerosAleatorios } from './components/NumerosAleatorios'
+
+function App() {
+  
+
+  return (
+    <>
+      <NumerosAleatorios/>
+    </>
+  )
+}
+
+export default App
---
+++ prueba/src/components/NumerosAleatorios.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export function NumerosAleatorios() {
+    const [numeroRandom1, setNumeroRandom1] = useState(-1)
+    const [numeroRandom2, setNumeroRandom2] = useState(-2)
+
+    const aleatorio = () => {
+        const n1 = Math.floor(Math.random() * 10)
+        const n2 = Math.floor(Math.random() * 10)
+
+        setNumeroRandom1(n1)
+        setNumeroRandom2(n2)
+    }
+
+    return (
+        <>
+            <div>
+                <h1>Números Aleatoris</h1>
+                {numeroRandom1 >= 0 ? (
+                    <>
+                        <p>Número: {numeroRandom1}</p>
+                        <p>Número: {numeroRandom2}</p>
+                    </>) : ''}
+
+                <button onClick={aleatorio}>Generar Números</button>
+                {numeroRandom1 === numeroRandom2 ? <h2>Los Números son Iguales</h2> : ''}
+            </div>
+        </>
+    )
+}
